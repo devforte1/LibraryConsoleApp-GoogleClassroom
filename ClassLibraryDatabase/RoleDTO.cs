a2e30e4b2c7902a4a421836cd566ae8636a7f33e
devforte1 <devforte@gmail.com>
2022-04-16 13:54:42 -0700

    Add DTO's to GitHub repository.

---
+++ ClassLibraryDatabase/RoleDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryDatabase
+{
+    public class RoleDTO
+    {
+        public RoleDTO()
+        {
+            // TODO: Implement RoleDTO() constructor.
+        }
+
+    }
+}
---
+++ ClassLibraryDatabase/UserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryDatabase
+{
+    public class UserDTO
+    {
+        private string _userName;
+
+        public string Name { get => _userName; set => _userName = value; }
+
+        public UserDTO(string userName)
+        {
+            _userName = userName;
+        }
+    }
+}
