1e922bf0fa805706398442992f4ee738771b664e
devforte1 <devforte@gmail.com>
2022-04-16 13:51:40 -0700

    Update DTO's for initial structure.

--- ConsoleLibrary/Program.cs
+++ ConsoleLibrary/Program.cs
@@ -4,17 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ClassLibraryCommon;
+using ClassLibraryDatabase;
+
 namespace ConsoleLibrary
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            RoleDTO roleAdmin = new RoleDTO();
+            UserDTO user1 = new UserDTO("user1");
         }
 
         static void CreateUser()
         {
             // TODO: Implement CreateUser().
+            
         }
 
         static void CreateRole()
